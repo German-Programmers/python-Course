2cb4e6a248631057dc4da0824daf0571122d1557
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-05-02 22:33:55 +0200

    first commit

---
+++ 1-python-start/app.py
@@ -0,0 +1,15 @@
+x = 9
+y = 5
+z = x / y
+
+#  + - / * operations
+
+h = "Hello German Programmers"
+check = False
+g = str(x)
+firstName = "Ahmad"
+lastName = "Osman"
+print(firstName + " " + lastName)
+
+print(x**3)
+
---
+++ 1-python-start/if-statement.py
@@ -0,0 +1,14 @@
+
+grade = 49
+if grade >= 50:
+    print("Success")
+else:
+    print("Fail")
+
+# >100 or <0   print error
+# >=90  print Excellent
+# [80 - 90[  print very good
+# [70 - 80[ print good
+# [60 - 70[ print middle
+# [50 - 60[ print weak
+# < 50 print Fail
